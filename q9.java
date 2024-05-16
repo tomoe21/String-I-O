09a67b9a9fe75995c8397316cc4823e2b4a3c51d
tomoe21 <147045040+tomoe21@users.noreply.github.com>
2024-05-16 13:48:10 +0530

    Update q9.java

--- q9.java
+++ q9.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class q9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Request a sentence and two words from the user
         System.out.print("Enter a sentence: ");
         String sentence = scanner.nextLine();
         System.out.print("Enter a word to search for: ");
@@ -12,13 +10,10 @@ public class q9 {
         System.out.print("Enter a word to replace it with: ");
         String replaceWord = scanner.nextLine();
 
-        // Find the first occurrence of the search word in the sentence
         int index = sentence.indexOf(searchWord);
 
-        // Replace the word using substring operations and concatenation
         String modifiedSentence = sentence.substring(0, index) + replaceWord + sentence.substring(index + searchWord.length());
 
-        // Display the original and the modified sentences
         System.out.println("\nOriginal Sentence: " + sentence);
         System.out.println("Modified Sentence: " + modifiedSentence);
 
