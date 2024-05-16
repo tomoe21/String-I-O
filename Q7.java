205d3bef8352e027f944106315b0e4fe2cb110a9
tomoe21 <147045040+tomoe21@users.noreply.github.com>
2024-05-16 13:46:45 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -3,19 +3,14 @@ import java.util.Scanner;
 public class Q7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Prompt user to enter a string
         System.out.print("Enter a string: ");
         String inputString = scanner.nextLine();
 
-        // Convert the string to a character array
         char[] charArray = inputString.toCharArray();
 
-        // Prompt user to enter a character to search
         System.out.print("Enter a character to search in the string: ");
         char searchChar = scanner.next().charAt(0);
 
-        // Find the first and last occurrences of the character
         int firstOccurrence = -1;
         int lastOccurrence = -1;
         for (int i = 0; i < charArray.length; i++) {
@@ -27,7 +22,6 @@ public class Q7 {
             }
         }
 
-        // Display the character array and the positions found (if any)
         System.out.println("\nCharacter Array:");
         System.out.println(charArray);
 
