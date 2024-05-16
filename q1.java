df64590e4a8f8dd05096c31a03bab73b76c185cb
tomoe21 <147045040+tomoe21@users.noreply.github.com>
2024-05-16 13:42:48 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,30 +1,23 @@
 public class q1 {
     public static void main(String[] args) {
-        // Using String literals
-        String str1 = "hello";
-        String str2 = "hello";
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = new String("hello");
 
-        // Using new keyword
-        String str3 = new String("hello");
-        String str4 = new String("hello");
-
-        // Checking memory addresses
         System.out.println("Memory addresses using String literals:");
-        System.out.println("str1 address: " + System.identityHashCode(str1));
-        System.out.println("str2 address: " + System.identityHashCode(str2));
+        System.out.println("s1 address: " + System.identityHashCode(s1));
+        System.out.println("s2 address: " + System.identityHashCode(s2));
 
         System.out.println("\nMemory addresses using new keyword:");
-        System.out.println("str3 address: " + System.identityHashCode(str3));
-        System.out.println("str4 address: " + System.identityHashCode(str4));
+        System.out.println("s3 address: " + System.identityHashCode(s3));
 
-        // String comparison
         System.out.println("\nString comparison using equals() method:");
-        System.out.println("str1.equals(str2): " + str1.equals(str2)); // true
-        System.out.println("str1.equals(str3): " + str1.equals(str3)); // true
+        System.out.println("s1.equals(s2): " + s1.equals(s2));
+        System.out.println("s1.equals(s3): " + s1.equals(s3));
 
         System.out.println("\nString comparison using == operator:");
-        System.out.println("str1 == str2: " + (str1 == str2)); // true
-        System.out.println("str1 == str3: " + (str1 == str3)); // false
+        System.out.println("s1 == s2: " + (s1 == s2)); 
+        System.out.println("s1 == s3: " + (s1 == s3)); 
     }
 }
 
